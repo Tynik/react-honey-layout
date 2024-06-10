b17cf4dd92d119399a12dca958f95372f0f3348c
Mykhailo Aliinyk <m.aliynik@gmail.com>
2024-06-10 22:19:04 +0100

    0.4.1-beta - Trying to use different styled component declaration

--- src/components/HoneyBox.ts
+++ src/components/HoneyBox.ts
@@ -1,11 +1,10 @@
 import styled, { css } from 'styled-components';
 
 import type { HoneyBoxProps } from '../types';
-
 import { generateMediaStyles, generateStyles } from '../helpers';
 import { HTML_ATTRIBUTES } from '../constants';
 
-export const HoneyBox = styled.div.withConfig({
+export const HoneyBox = styled('div').withConfig({
   shouldForwardProp: prop =>
     HTML_ATTRIBUTES.includes(prop as never) ||
     prop.startsWith('on') ||
